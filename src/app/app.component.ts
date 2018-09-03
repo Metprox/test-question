1fd9f143cf5c2e433bef75f5d86aa64bf47d875a
Oleg Vakarchuk <yuzhnybro@gmail.com>
2018-09-03 17:19:56 +0300

    hello guys

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'swedavia';
+
+  names: string;
+
+  onNames(event: any) {
+    this.names = event.target.value;
+  }
+
+  animation() {
+    const anima = document.querySelector('aside');
+    anima.className = 'helloAnim';
+    console.log(anima.className);
+    console.log(anima);
+    anima.style.transform = '(0, 0)';
+  }
 }
